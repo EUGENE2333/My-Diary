980c575bbf5bd579a17572d6784ae12f19d079cc
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-01-04 09:11:20 +0300

    fixed error

--- app/src/main/java/com/example/mydiary/data/mapper/DomainMapper.kt
+++ app/src/main/java/com/example/mydiary/data/mapper/DomainMapper.kt
@@ -1,6 +1,6 @@
 package com.example.mydiary.data.mapper
 
 interface DomainMapper<Entity,DomainModel>{
-    fun mapToDomain(entity:git cmmit Entity): DomainModel
+    fun mapToDomain(entity: Entity): DomainModel
     fun mapFromDomain(domainModel: DomainModel): Entity
 }
