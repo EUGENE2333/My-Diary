4ad9ec409eec4af6ed56e6f9d6ffc424336b43c8
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-01-11 22:17:01 +0300

    add notes network datasource interface

--- app/src/main/java/com/example/mydiary/network/NotesNetworkDatasource.kt
+++ app/src/main/java/com/example/mydiary/network/NotesNetworkDatasource.kt
@@ -1,4 +1,8 @@
 package com.example.mydiary.network
 
-class NotesNetworkDatasource {
+import com.example.mydiary.network.module.NetworkNotes
+
+interface NotesNetworkDatasource {
+    suspend fun getNotes(range: String): List<NetworkNotes>
+    suspend fun getSpecificNote(id: String): List<NetworkNotes>
 }
