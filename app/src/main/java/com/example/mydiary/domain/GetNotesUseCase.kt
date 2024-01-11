69e1131f086447e81b926bba415ed9e7a1465eab
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-01-11 23:51:27 +0300

    add get specific note

--- app/src/main/java/com/example/mydiary/domain/GetNotesUseCase.kt
+++ app/src/main/java/com/example/mydiary/domain/GetNotesUseCase.kt
@@ -1,9 +1,10 @@
-package com.example.mydiary.domain.repository
+package com.example.mydiary.domain
 
 import com.example.mydiary.data.model.Notes
 import com.example.mydiary.data.repository.NotesRepository
 import com.example.mydiary.data.repository.Resources
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 abstract class GetNotesUseCase(
     private val notesRepository: NotesRepository
@@ -11,4 +12,8 @@ abstract class GetNotesUseCase(
     operator fun invoke(range: String): Flow<Resources<List<Notes>>> =
         notesRepository.getNotesStream(range)
 
+    fun getSpecificNote(id: String): Flow<Resources<Notes>> =  flow {
+        val data = notesRepository.getSpecificNote(id)
+        emit(data)
+    }
 }
