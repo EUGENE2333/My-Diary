b54abc9048e220567b5f62ad246a073809b6a35b
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-01-11 18:31:55 +0300

    fun to convert network model to local model

--- app/src/main/java/com/example/mydiary/data/mapper/NotesMapper.kt
+++ app/src/main/java/com/example/mydiary/data/mapper/NotesMapper.kt
@@ -1,2 +1,17 @@
 package com.example.mydiary.data.mapper
 
+import com.example.mydiary.database.model.NotesEntity
+import com.example.mydiary.network.NetworkNotes
+
+/**
+ * Converts the network model to the local model for persisting
+ * by the local data source
+ */
+fun NetworkNotes.asEntity() = NotesEntity(
+     userId = userId,
+     title = title,
+     description = description,
+     timestamp  = timestamp,
+     colorIndex = colorIndex,
+     documentId  = documentId
+)
