88efbde1babd90d9533db11aac9f29640dd40112
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-01-11 22:03:26 +0300

    update notes repo impl

--- app/src/main/java/com/example/mydiary/data/repository/NotesRepositoryImpl.kt
+++ app/src/main/java/com/example/mydiary/data/repository/NotesRepositoryImpl.kt
@@ -1,11 +1,21 @@
 package com.example.mydiary.data.repository
 
+import com.example.mydiary.data.mapper.asExternalModel
+import com.example.mydiary.data.model.Notes
 import com.example.mydiary.database.NotesDao
+import com.example.mydiary.database.model.NotesEntity
 import com.example.mydiary.network.NotesNetworkDatasource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class NotesRepositoryImpl(
     private val notesDao: NotesDao,
     private val network: NotesNetworkDatasource
-) {
+): NotesRepository{
 
+    override fun getNotesStream(): Flow<List<Notes>> =
+        notesDao.getNotesEntitiesAsFlow()
+            .map {
+                it.map(NotesEntity::asExternalModel)
+            }
 }
