4f068500c63b700121a7263ea0151bb457fb71ca
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-01-11 22:26:18 +0300

    minor change

--- app/src/main/java/com/example/mydiary/data/repository/NotesRepositoryImpl.kt
+++ app/src/main/java/com/example/mydiary/data/repository/NotesRepositoryImpl.kt
@@ -5,17 +5,25 @@ import com.example.mydiary.data.model.Notes
 import com.example.mydiary.database.NotesDao
 import com.example.mydiary.database.model.NotesEntity
 import com.example.mydiary.network.NotesNetworkDatasource
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 
 class NotesRepositoryImpl(
     private val notesDao: NotesDao,
-    private val network: NotesNetworkDatasource
+    private val network: NotesNetworkDatasource,
+    private val ioDispatcher: CoroutineDispatcher,
 ): NotesRepository{
 
-    override fun getNotesStream(): Flow<List<Notes>> =
+    override fun getNotesStream(): Flow<List<Notes>> = flow {
+        val notesResults = withContext(ioDispatcher){
+
+        }
         notesDao.getNotesEntitiesAsFlow()
             .map {
                 it.map(NotesEntity::asExternalModel)
             }
+    }
 }
