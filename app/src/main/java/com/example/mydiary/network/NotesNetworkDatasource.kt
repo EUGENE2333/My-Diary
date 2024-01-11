7ac5e766e6640542e06c0a682957aad615a76ae6
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-01-11 23:15:36 +0300

    get notes from remote implementation

--- app/src/main/java/com/example/mydiary/network/NotesNetworkDatasource.kt
+++ app/src/main/java/com/example/mydiary/network/NotesNetworkDatasource.kt
@@ -1,8 +1,10 @@
 package com.example.mydiary.network
 
+import com.example.mydiary.data.repository.Resources
 import com.example.mydiary.network.module.NetworkNotes
+import kotlinx.coroutines.flow.Flow
 
 interface NotesNetworkDatasource {
-    suspend fun getNotes(range: String): List<NetworkNotes>
+    suspend fun getNotes(userId:String): Flow<Resources<List<NetworkNotes>>>
     suspend fun getSpecificNote(id: String): List<NetworkNotes>
 }
