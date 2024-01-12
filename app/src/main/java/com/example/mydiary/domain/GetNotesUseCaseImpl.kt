9bf98edf982f14618614a1c2961d51b69b6c30ca
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-01-12 10:07:31 +0300

    Update usecase

--- app/src/main/java/com/example/mydiary/domain/GetNotesUseCaseImpl.kt
+++ app/src/main/java/com/example/mydiary/domain/GetNotesUseCaseImpl.kt
@@ -1,16 +1,17 @@
 package com.example.mydiary.domain
 
 import com.example.mydiary.data.model.Notes
+import com.example.mydiary.data.repository.NotesRepository
 import com.example.mydiary.data.repository.Resources
 import com.example.mydiary.domain.repository.GetNotesUseCase
 import kotlinx.coroutines.flow.Flow
 
-class GetNotesUseCaseImpl: GetNotesUseCase {
+class GetNotesUseCaseImpl(  private val notesRepository: NotesRepository): GetNotesUseCase {
     override fun getAllNotes(range: String): Flow<Resources<List<Notes>>> {
-        TODO("Not yet implemented")
+     return notesRepository.getNotesStream(range)
     }
 
     override suspend fun getSpecificNote(id: String): Resources<Notes> {
-        TODO("Not yet implemented")
+       return notesRepository.getSpecificNote(id)
     }
 }
