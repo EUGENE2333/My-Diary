dea2863e178f013ef85d413127ee23858807ec1c
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-01-27 23:25:38 +0300

    added tryMapToRemote function

--- app/src/main/java/com/example/mydiary/data/mapper/NotesRemoteMapper.kt
+++ app/src/main/java/com/example/mydiary/data/mapper/NotesRemoteMapper.kt
@@ -3,6 +3,7 @@ package com.example.mydiary.data.mapper
 import android.util.Log
 import com.example.mydiary.database.model.NotesEntity
 import com.example.mydiary.network.module.NetworkNotes
+import com.google.firebase.Timestamp
 
 class NotesRemoteMapper():RemoteMapper<NetworkNotes,NotesEntity?> {
     override fun mapFromRemote(remote: NetworkNotes): NotesEntity? {
@@ -15,7 +16,7 @@ class NotesRemoteMapper():RemoteMapper<NetworkNotes,NotesEntity?> {
     }
 
     override fun mapToRemote(local: NotesEntity?): NetworkNotes {
-        TODO("Not yet implemented")
+        return tryMapToRemote(local = local!!)
     }
 
     private fun tryMapFromRemote(netWorkNotes: NetworkNotes): NotesEntity {
@@ -25,6 +26,19 @@ class NotesRemoteMapper():RemoteMapper<NetworkNotes,NotesEntity?> {
                 title = title,
                 description = description,
                 timestamp = timestamp.seconds,
+                colorIndex = colorIndex,
+                documentId = documentId
+            )
+        }
+    }
+
+    private fun tryMapToRemote(local: NotesEntity): NetworkNotes {
+        return with(local) {
+            NetworkNotes(
+                userId = userId,
+                title = title,
+                description = description,
+                timestamp = Timestamp(timestamp, 0),
                 colorIndex = colorIndex,
                 documentId = documentId
             )
