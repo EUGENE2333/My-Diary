9e20c635c8bbed78e3daa4664e7424bb2fd9d4f3
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-01-27 23:27:36 +0300

    default values for Network model

--- app/src/main/java/com/example/mydiary/network/module/NetworkNotes.kt
+++ app/src/main/java/com/example/mydiary/network/module/NetworkNotes.kt
@@ -4,10 +4,10 @@ import com.google.firebase.Timestamp
 
 
 data class NetworkNotes(
-    val userId: String ,
-    val title: String,
-    val description: String,
-    val timestamp: Timestamp,
-    val colorIndex: Int,
-    val documentId: String
+    val userId: String = "",
+    val title: String = "",
+    val description: String = "",
+    val timestamp: Timestamp = Timestamp.now(),
+    val colorIndex: Int = 0,
+    val documentId: String = ""
 )
