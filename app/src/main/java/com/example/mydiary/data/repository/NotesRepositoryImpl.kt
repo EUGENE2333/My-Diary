83677df3a77b86632af339882e0621d24d4a4c36
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-01-31 01:09:47 +0300

    Update method to update local entries

--- app/src/main/java/com/example/mydiary/data/repository/NotesRepositoryImpl.kt
+++ app/src/main/java/com/example/mydiary/data/repository/NotesRepositoryImpl.kt
@@ -18,36 +18,39 @@ class NotesRepositoryImpl(
     private val ioDispatcher: CoroutineDispatcher,
     private val notesRemoteMapper: NotesRemoteMapper,
     private val notesDomainMapper: NotesDomainMapper
-): NotesRepository{
+): NotesRepository {
 
     override fun getNotesStream(): Flow<List<Notes>> = notesDao.getNotesEntitiesAsFlow()
-        .map {entities ->
+        .map { entities ->
             entities.mapNotNull { notesDomainMapper.mapToDomain(it) }
 
         }
         .flowOn(ioDispatcher)
 
-    override suspend fun getSpecificNote(userId: String): Resources<Notes> = withContext(ioDispatcher){
-        val localModel =  notesDao.getNoteEntityById(userId)
-        Resources.Success(notesDomainMapper.mapToDomain(localModel))
-    }
+    override suspend fun getSpecificNote(userId: String): Resources<Notes> =
+        withContext(ioDispatcher) {
+            val localModel = notesDao.getNoteEntityById(userId)
+            Resources.Success(notesDomainMapper.mapToDomain(localModel))
+        }
 
     override suspend fun syncNotesFromNetwork(userId: String) {
         network.getNotes(userId)
-            .collect{
-                when(it){
+            .collect {
+                when (it) {
                     is Resources.Success -> {
                         val networkNotesList = it.data
-                        val localNotesList = networkNotesList?.mapNotNull{
+                        val localNotesList = networkNotesList?.mapNotNull {
                             notesRemoteMapper.mapFromRemote(it)
                         }
                         if (localNotesList != null) {
                             notesDao.insertNotes(localNotesList)
                         }
                     }
+
                     is Resources.Error -> {
 
                     }
+
                     is Resources.Loading -> {
 
                     }
@@ -56,30 +59,37 @@ class NotesRepositoryImpl(
     }
 
     override suspend fun saveNotesToLocal(domainNotes: Notes) {
-        withContext(ioDispatcher){
+        withContext(ioDispatcher) {
             notesDomainMapper.mapFromDomain(domainNotes)
         }
     }
 
     override suspend fun saveNotesToRemote(userId: String) {
         val localNotesList = notesDao.getNotesEntitiesAsFlow().firstOrNull() ?: emptyList()
-            val networkNotesList = localNotesList.map{ notesRemoteMapper.mapToRemote(localNotesList) }
+        val networkNotesList = localNotesList.map { notesRemoteMapper.mapToRemote(localNotesList) }
 
     }
 
-    override suspend fun deleteNote(note: Notes): Resources<Unit> = withContext(ioDispatcher){
+    override suspend fun deleteNote(note: Notes): Resources<Unit> = withContext(ioDispatcher) {
         try {
             val noteEntity = notesDomainMapper.mapFromDomain(note)
             noteEntity?.let { notesDao.deleteNotes(it) }
 
             Resources.Success(Unit)
 
-        } catch (e:Exception){
+        } catch (e: Exception) {
             Resources.Error(e)
         }
     }
 
-    override suspend fun updateNote(note: Notes): Resources<Unit> {
-        TODO("Not yet implemented")
+    override suspend fun updateNote(note: Notes): Resources<Unit> = withContext(ioDispatcher) {
+        try {
+            val noteEntity = notesDomainMapper.mapFromDomain(note)
+            noteEntity?.let { notesDao.updateNotes(it) }
+
+            Resources.Success(Unit)
+        } catch (e: Exception) {
+            Resources.Error(e)
+        }
     }
 }
