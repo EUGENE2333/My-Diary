0365cbfb20a66a866202f30520345a12f3e6ce35
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-01-30 22:36:32 +0300

    Update getNotesStream

--- app/src/main/java/com/example/mydiary/data/repository/NotesRepositoryImpl.kt
+++ app/src/main/java/com/example/mydiary/data/repository/NotesRepositoryImpl.kt
@@ -2,14 +2,12 @@ package com.example.mydiary.data.repository
 
 import com.example.mydiary.data.mapper.NotesDomainMapper
 import com.example.mydiary.data.mapper.NotesRemoteMapper
-import com.example.mydiary.data.mapper.asExternalModel
 import com.example.mydiary.data.model.Notes
 import com.example.mydiary.database.NotesDao
-import com.example.mydiary.database.model.NotesEntity
 import com.example.mydiary.network.NotesNetworkDatasource
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 
@@ -21,10 +19,12 @@ class NotesRepositoryImpl(
     private val notesDomainMapper: NotesDomainMapper
 ): NotesRepository{
 
-    override fun getNotesStream(): Flow<List<Notes>> = flow {
-        notesDao.getNotesEntitiesAsFlow()
-            .map { it.map(NotesEntity::asExternalModel) }
-    }
+    override fun getNotesStream(): Flow<List<Notes>> = notesDao.getNotesEntitiesAsFlow()
+        .map {entities ->
+            entities.mapNotNull { notesDomainMapper.mapToDomain(it) }
+
+        }
+        .flowOn(ioDispatcher)
 
     override suspend fun getSpecificNote(userId: String): Resources<Notes> = withContext(ioDispatcher){
         val localModel =  notesDao.getNoteEntityById(userId)
