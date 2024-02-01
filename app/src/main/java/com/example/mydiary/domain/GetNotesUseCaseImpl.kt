413f135a89192934c57a289f91978a909c44a6d0
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-02-01 22:24:09 +0300

    Monor change

--- app/src/main/java/com/example/mydiary/domain/GetNotesUseCaseImpl.kt
+++ app/src/main/java/com/example/mydiary/domain/GetNotesUseCaseImpl.kt
@@ -1,14 +1,14 @@
 package com.example.mydiary.domain
 
 import com.example.mydiary.data.model.Notes
-import com.example.mydiary.database.localRepository.NotesRepository
 import com.example.mydiary.data.repository.Resources
+import com.example.mydiary.database.localRepository.NotesRepository
 import com.example.mydiary.domain.repository.GetNotesUseCase
 import kotlinx.coroutines.flow.Flow
 
 class GetNotesUseCaseImpl(  private val notesRepository: NotesRepository): GetNotesUseCase {
-    override fun getAllNotes(range: String): Flow<Resources<List<Notes>>> {
-     return notesRepository.getNotesStream(range)
+    override fun getAllNotes(range: String): Flow<List<Notes>> {
+     return notesRepository.getNotesStream()
     }
 
     override suspend fun getSpecificNote(id: String): Resources<Notes> {
