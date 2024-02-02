7c8dbc9fc6e71f283ac8e96e4ba63eb401d916df
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-02-02 07:38:05 +0300

    minor change

--- app/src/main/java/com/example/mydiary/database/localRepository/NotesRepository.kt
+++ app/src/main/java/com/example/mydiary/database/localRepository/NotesRepository.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface NotesRepository {
     fun getNotesStream():Flow<List<Notes>>
-    suspend fun getSpecificNote(userId: String): Resources<Notes>
+    suspend fun getSpecificNote(documentId: String): Resources<Notes>
     suspend fun syncNotesFromNetwork(userId: String)
     suspend fun saveNotesToLocal(domainNotes: Notes)
     suspend fun saveNotesToRemote(userId:String)
