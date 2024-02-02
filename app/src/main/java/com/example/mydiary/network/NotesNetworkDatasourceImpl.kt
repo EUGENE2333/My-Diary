ff0599a59768b48785fb2784f97f6567c568f2fb
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-02-02 09:14:43 +0300

    Override method delete account

--- app/src/main/java/com/example/mydiary/network/NotesNetworkDatasourceImpl.kt
+++ app/src/main/java/com/example/mydiary/network/NotesNetworkDatasourceImpl.kt
@@ -1,12 +1,16 @@
 package com.example.mydiary.network
 
+import android.app.AlertDialog
+import android.content.Context
 import com.example.mydiary.data.repository.Resources
 import com.example.mydiary.network.module.NetworkNotes
 import com.google.firebase.Timestamp
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.Query
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
@@ -100,5 +104,42 @@ class NotesNetworkDatasourceImpl:NotesNetworkDatasource {
             .addOnCompleteListener {
                 onComplete.invoke(it.isSuccessful)
             }
+    }
+
+    override fun deleteAccount(context: Context) {
+        val user = Firebase.auth.currentUser
+
+        // Check if the user is currently authenticated
+        if (user != null) {
+            // Create an instance of AlertDialog.Builder
+            val builder = AlertDialog.Builder(context)
+
+            // Set the dialog title, message, and button texts
+            builder.setTitle("Confirm Account Deletion")
+            builder.setMessage("Are you sure you want to delete your account? This action is irreversible." +
+                    " All your account data will be permanently deleted.")
+            builder.setPositiveButton("Delete") { _, _ ->
+                // User confirmed, proceed with account deletion
+                user.delete()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            // Account deletion successful
+                            // You can perform any additional actions here, such as navigating to a different screen
+                         //   navToSignUpPage.invoke()
+
+                        } else {
+                            // Account deletion failed
+                            // You can handle the error here or display an error message to the user
+                        }
+                    }
+            }
+            builder.setNegativeButton("Cancel") { _, _ ->
+                // User canceled the deletion, do nothing or show a message
+            }
+
+            // Show the dialog
+            val dialog = builder.create()
+            dialog.show()
+        }
     }
 }
