5ce1bcdf30c73833ef5bbca686ab9c56e02500f6
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-02-02 08:57:44 +0300

    Add function to add note

--- app/src/main/java/com/example/mydiary/network/NotesNetworkDatasource.kt
+++ app/src/main/java/com/example/mydiary/network/NotesNetworkDatasource.kt
@@ -2,11 +2,19 @@ package com.example.mydiary.network
 
 import com.example.mydiary.data.repository.Resources
 import com.example.mydiary.network.module.NetworkNotes
+import com.google.firebase.Timestamp
 import kotlinx.coroutines.flow.Flow
 
 interface NotesNetworkDatasource {
     suspend fun getNotes(userId:String): Flow<Resources<List<NetworkNotes>>>
-    suspend fun addNotes(userId: String,notesList:List<NetworkNotes>)
+    fun addNote(
+        userId: String,
+        title: String,
+        description: String,
+        timestamp: Timestamp,
+        color: Int,
+        onComplete: (Boolean) -> Unit
+    )
     suspend fun updateNote(
         noteId:String,
         title: String,
