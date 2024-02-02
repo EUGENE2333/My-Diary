accb9fe8c934d4f8cf8040dbed96df10df91849d
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-02-02 11:17:17 +0300

    Add addNote function

--- app/src/main/java/com/example/mydiary/domain/NotesUseCase.kt
+++ app/src/main/java/com/example/mydiary/domain/NotesUseCase.kt
@@ -3,6 +3,7 @@ package com.example.mydiary.domain
 import com.example.mydiary.data.model.Notes
 import com.example.mydiary.data.repository.Resources
 import com.example.mydiary.database.localRepository.NotesRepository
+import com.google.firebase.Timestamp
 import kotlinx.coroutines.flow.Flow
 
 class NotesUseCase(
@@ -11,4 +12,26 @@ class NotesUseCase(
     fun getAllNotes(): Flow<List<Notes>> = notesRepository.getNotesStream()
 
    suspend fun getSpecificNote(documentId: String): Resources<Notes> = notesRepository.getSpecificNote(documentId)
+
+    suspend fun addNote(
+        userId: String,
+        title: String,
+        description: String,
+        timestamp: Timestamp,
+        color: Int = 0
+    ){
+        try {
+            val localNote = Notes(
+                userId = userId,
+                title = title,
+                description = description,
+                timestamp = timestamp,
+                colorIndex = color
+            )
+            notesRepository.saveNotesToLocal(localNote)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
