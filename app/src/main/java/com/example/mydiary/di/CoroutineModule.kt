0a6ac40446adea41384eed01f4ade9924804327d
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-02-18 19:39:22 +0300

    Add annotation classes

--- app/src/main/java/com/example/mydiary/di/CoroutineModule.kt
+++ app/src/main/java/com/example/mydiary/di/CoroutineModule.kt
@@ -2,9 +2,22 @@ package com.example.mydiary.di
 
 import dagger.Module
 import dagger.hilt.InstallIn
+import javax.inject.Qualifier
 
 @InstallIn
 @Module
 object CoroutineModule {
 
 }
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IoDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainDispatcher
