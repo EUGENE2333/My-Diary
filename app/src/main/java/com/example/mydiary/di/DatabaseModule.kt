5c2427310dde409d1109d8d96e1f3c07ee852517
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-02-18 20:13:08 +0300

    Added DatabaseModule

---
+++ app/src/main/java/com/example/mydiary/di/DatabaseModule.kt
@@ -0,0 +1,24 @@
+package com.example.mydiary.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.mydiary.database.NotesDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@InstallIn
+@Module
+object DatabaseModule {
+  @Singleton
+  @Provides
+  fun providesDatabase(@ApplicationContext context: Context): NotesDatabase{
+    return Room.databaseBuilder(
+      context.applicationContext,
+      NotesDatabase::class.java,
+      "Notes.db"
+    ).build()
+  }
+}
