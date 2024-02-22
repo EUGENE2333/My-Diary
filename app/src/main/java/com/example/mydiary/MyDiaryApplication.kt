aec1af7ac230bc39716d0e71ca5da3db302a089c
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-02-22 23:24:22 +0300

    update application class

--- app/src/main/java/com/example/mydiary/MyDiaryApplication.kt
+++ app/src/main/java/com/example/mydiary/MyDiaryApplication.kt
@@ -1,30 +1,36 @@
 package com.example.mydiary
 
 import android.app.Application
-import com.example.mydiary.data.utils.PasswordManager
-import com.example.mydiary.data.utils.PreferencesManager
-import com.example.mydiary.domain.repository.AuthRepository
-import com.example.mydiary.presentation.DiaryViewModelFactory
+import dagger.hilt.android.HiltAndroidApp
 
-
+@HiltAndroidApp
 class MyDiaryApplication: Application() {
 
-    val passwordManager: PasswordManager by lazy { PasswordManager(this) }
+    //  val passwordManager: PasswordManager by lazy { PasswordManager(this) }
 
-    private val authRepository: AuthRepository by lazy { AuthRepository()}
+    //  private val authRepository: AuthRepository by lazy { AuthRepository()}
 
-    private val preferencesManager: PreferencesManager by lazy { PreferencesManager(this) }
+    //  private val preferencesManager: PreferencesManager by lazy { PreferencesManager(this) }
 
-    val diaryViewModelFactory by lazy {
+    //   private val storageRepository: StorageRepository by lazy {StorageRepository()}
+
+    //  private val notesUsecase: NotesUseCase by lazy {NotesUseCase()}
+
+    /* val diaryViewModelFactory by lazy {
         DiaryViewModelFactory(
             this,
             authRepository,
             preferencesManager,
 
 
-        )
-    }
+        ) */
 
+    /* val detailViewModelFactory by lazy {
+        DetailViewModelFactory(
+            storageRepository,
+            notesRepository
+        )
+    } */
 }
 
 
