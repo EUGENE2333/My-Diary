0752c4330d54a437704fca7739f66b20d67b2bed
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-12-23 21:24:11 +0300

    update

--- app/src/main/java/com/example/mydiary/ui/theme/Color.kt
+++ app/src/main/java/com/example/mydiary/ui/theme/Color.kt
@@ -1,28 +1,7 @@
-
-
 package com.example.mydiary.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
 
-class Color {
-    val Red500 = Color(0xFFF44336)
-    val Pink500 = Color(0xFFE91E63)
-    val Purple500 = Color(0xFF9C27B0)
-    val DeepPurple500 = Color(0xFF673AB7)
-    val Indigo500 = Color(0xFF3F51B5)
-    val Blue500 = Color(0xFF2196F3)
-    val LightBlue500 = Color(0xFF03A9F4)
-    val Cyan500 = Color(0xFF00BCD4)
-    val Teal500 = Color(0xFF009688)
-    val Green500 = Color(0xFF4CAF50)
-    val LightGreen500 = Color(0xFF8BC34A)
-    val Lime500 = Color(0xFFCDDC39)
-    val Yellow500 = Color(0xFFFFEB3B)
-    val Amber500 = Color(0xFFFFC107)
-    val Orange500 = Color(0xFFFF9800)
-    val DeepOrange500 = Color(0xFFFF5722)
-    val Brown500 = Color(0xFF795548)
-    val Grey500 = Color(0xFF9E9E9E)
-    val BlueGrey500 = Color(0xFF607D8B)
-}
+val crownColor = Color(0xFFFFb743)
+ val Corn = Color(0xFFFFF799)
