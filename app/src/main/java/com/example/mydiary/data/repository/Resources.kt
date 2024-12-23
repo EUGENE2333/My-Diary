90a383a579c45a3bf232a9538c890d3d53baa14a
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-12-23 21:19:17 +0300

    update

--- app/src/main/java/com/example/mydiary/data/repository/Resources.kt
+++ app/src/main/java/com/example/mydiary/data/repository/Resources.kt
@@ -1,6 +1,7 @@
 package com.example.mydiary.data.repository
 
 import android.util.Log
+import com.example.mydiary.data.model.User
 import com.example.mydiary.data.repository.Resources.*
 
 sealed class Resources<T>(
@@ -10,21 +11,4 @@ sealed class Resources<T>(
     class Loading<T>: Resources<T>()
     class Success<T>(data: T?): Resources<T>(data = data)
     class Error<T>(throwable: Throwable?): Resources<T>(throwable = throwable)
-}
-
-
-/**
- * Wraps api calls into a [Resources]. If the call throws an exception, then the exception is wrapped into an [AppError].
- *
- * @param apiCall the api call passed as a lambda function
- * @param T is the api call response type
- */
-suspend fun <T> executeRequest(apiCall: suspend () -> T): Resources<T> {
-    return try {
-        Success(apiCall())
-    } catch (e: Throwable) {
-        Log.d("executeRequest", e.toString())
-       Error(e)
-
-    }
 }
