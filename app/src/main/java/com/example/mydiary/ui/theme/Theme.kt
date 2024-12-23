34c9dd5cd97a09fb3c3606c3840923a26ba5314d
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-12-23 21:24:36 +0300

    update

--- app/src/main/java/com/example/mydiary/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mydiary/ui/theme/Theme.kt
@@ -4,7 +4,10 @@ import android.app.Activity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
 import androidx.compose.material.lightColors
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberUpdatedState
@@ -146,7 +149,17 @@ private val this3ColorPalette = lightColors(
     // Define other color values for the cyan color theme
 )
 
+/**
+* Use this in a component to disable colored ripple effect
+*/
+@Immutable
+object NoRippleTheme : RippleTheme {
+    @Composable
+    override fun defaultColor() = Color.Unspecified
 
+    @Composable
+    override fun rippleAlpha() = RippleAlpha(0f, 0f, 0f, 0f)
+}
 
 
 
