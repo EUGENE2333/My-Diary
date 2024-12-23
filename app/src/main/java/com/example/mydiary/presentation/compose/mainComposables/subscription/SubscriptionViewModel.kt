2d7c91e8cf725b1b30792f424f22aabd2e85ade3
EugeneLumbasi <eugenelumbasi254@gmail.com>
2024-12-23 21:23:08 +0300

    SubscriptionViewmodel.kt

--- app/src/main/java/com/example/mydiary/presentation/compose/mainComposables/subscription/SubscriptionViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mydiary.presentation.compose.mainComposables.subscription
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
-
-@HiltAndroidApp
-class SubscriptionViewModel @Inject constructor(): ViewModel(){
-}
