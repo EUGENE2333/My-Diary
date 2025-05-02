b56a734f2b95fdc563f4ffdcb114eb1995eed6ec
EugeneLumbasi <eugenelumbasi254@gmail.com>
2025-05-02 23:37:27 +0300

    update

--- app/src/main/java/com/example/mydiary/core/analytics/Property.kt
+++ app/src/main/java/com/example/mydiary/core/analytics/Property.kt
@@ -1,5 +1,5 @@
 package com.example.mydiary.core.analytics
-/*
+
 /**
  * Represents a property that can be attached to an analytics event or page.
  */
@@ -12,4 +12,3 @@ enum class Properties(override val propertyName: String) : Property {
     SUBSCRIPTION_PRICE("subscription_price"),
     SUBSCRIPTION_CURRENCY("subscription_currency"),
 }
-*/
